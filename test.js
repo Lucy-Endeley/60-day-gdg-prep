dbe5a73350098d4894bba6a484c451d2973e0b7a
Lucy <lucyakosua3239@gmail.com>
2026-01-03 02:50:42 +0000

    Day 1: First Successful code with template literals

---
+++ test.js
@@ -0,0 +1,10 @@
+//My first code in this project
+console.log("This is the first step to greatness");
+console.log("Join me on this wonderful journey");
+
+const myName = "Lucy";
+const school = "RMU" ;
+const MyGoal = "Qualify for GDG Application and grow technically";
+
+console.log(`My Name is ${myName} from ${school}`);
+console.log(`My Goal at the of this is to : ${MyGoal}`);
